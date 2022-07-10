860dc5150ca33bb3e719039900acee2be44cf7f4
sergei lauffenburger <lauffenburger@live.fr>
2022-07-10 02:01:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const txtAnim = document.querySelector("h1");
+
+new Typewriter(txtAnim, {
+    //loop: true,
+  deleteSpeed: 20,
+})
+  .changeDelay(20)
+  .typeString("Moi c'est John Doe")
+  .pauseFor(300)
+  .typeString("<strong>, Dev Full-stack</strong> !")
+  .pause(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #27ae60"> CSS</span> !')
+  .pauseFor(1000)
+  .deleteChars(5)
+  .typeString('<span style="color: midnightblue"> react</span> !')
+  .pauseFor(1000)
+  .deleteChars(7)
+  .typeString('<span style="color: #ea39ff"> PHP</span> !')
+  .pauseFor(1000)
+  .deleteChars(5)
+  .typeString('<span style="color: #ff6910"> Javascript</span> !')
+  .start();
